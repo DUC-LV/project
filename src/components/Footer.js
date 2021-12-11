07ecd814cbdcde4cf8d46a152dfc51ec80a97189
DucLV08 <duc080999@gmail.com>
2021-12-11 20:31:42 +0700

    add footer and image

---
+++ src/components/Footer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function Footer (){
+    return (
+        <div className="footer">
+            <div className="footer-head">
+                <h2 className="footer-head-title">Liên Kết</h2>
+                <a href="http://chinhphu.vn/portal/page/portal/chinhphu/trangchu">
+                    <img src="file:///C:/Users/Admin/Desktop/project/project/src/image/logo7.PNG" alt="" className="footer-head-pictrue" />
+                </a>
+                <a href="https://moh.gov.vn/">
+                    <img src="file:///C:/Users/Admin/Desktop/project/project/src/image/logo8.PNG" alt="" className="footer-head-pictrue" />
+                </a>
+                <a href="https://suckhoedoisong.vn/">
+                    <img src="file:///C:/Users/Admin/Desktop/project/project/src/image/logo9.PNG" alt="" className="footer-head-pictrue" />
+                </a>
+                <a href="https://giadinh.net.vn/">
+                    <img src="file:///C:/Users/Admin/Desktop/project/project/src/image/logo10.PNG" alt="" className="footer-head-pictrue" />
+                </a>
+                <a href="https://vccorp.vn/">
+                    <img src="file:///C:/Users/Admin/Desktop/project/project/src/image/logo11.PNG" alt="" className="footer-head-pictrue" />
+                </a>
+            </div>
+            <div className="footer-footer">
+                <img src="file:///C:/Users/Admin/Desktop/project/project/src/image/logo.png" alt="" className="footer-footer-pictrue" />
+                <h3 className="footer-footer-title">QUẢN LÝ NỘI DUNG: BÁO SỨC KHỎE VÀ ĐỜI SỐNG - CƠ QUAN NGÔN LUẬN CỦA BỘ Y TẾ</h3>
+                <p className="footer-footer-title1">Đường dây nóng:19009095</p>
+                <p className="footer-footer-title2">Email:covid19@suckhoedoisong.vn</p>
+            </div>
+      </div>
+    );
+}
+
+
+export default Footer;
